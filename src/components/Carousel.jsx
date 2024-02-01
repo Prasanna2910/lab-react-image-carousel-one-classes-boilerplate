5721d4608878a2a7343373b791e6bcafe731a867
Prasanna V <you@example.com>
2024-02-01 11:30:36 +0530

    coded

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,13 +1,46 @@
-import React, { Component } from "react";
-import "./Carousel.css";
-import { images } from "../data/CarouselData";
-// you can explore more - and check as how to use materiul ui
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
-import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import React, { Component } from 'react';
+import './Carousel.css';
+import { images } from '../data/CarouselData';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
-// implement the class below
 class Carousel extends Component {
-  
+  constructor() {
+    super();
+    this.state = {
+      index: 0,
+    };
+
+    this.imagesRoute = images[this.state.index].img;
+  }
+
+  handleIncrease = () => {
+    if (this.state.index === 2) {
+      this.setState({ index: 0 });
+    } else {
+      this.setState({ index: this.state.index + 1 });
+    }
+  };
+
+  handleDecrease = () => {
+    if (this.state.index === 0) {
+      this.setState({ index: 2 });
+    } else {
+      this.setState({ index: this.state.index - 1 });
+    }
+  };
+
+  render() {
+    this.imagesRoute = images[this.state.index].img;
+
+    return (
+      <div className="flex">
+        <ArrowBackIosIcon onClick={this.handleIncrease} />
+        <img src={this.imagesRoute} alt="carousel-img" className='images' />
+        <ArrowForwardIosIcon onClick={this.handleDecrease} />
+      </div>
+    );
+  }
 }
 
 export default Carousel;
